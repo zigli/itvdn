3bd3f4419d29da132683bff660f431add0c95805
ilgiz <4ilgiz@gmail.com>
2019-01-30 11:47:04 +0300

    Commit package3

---
+++ src/package4_what_is_null/Main.java
@@ -0,0 +1,22 @@
+package package4_what_is_null;
+
+import package3.Car;
+
+public class Main {
+    public static void main(String[] args) {
+        String str1 = null;
+        Car car1 = null;
+//        int i = null;
+
+        Integer i2 = null;
+        int i3 = i2;
+        System.out.println(i3);
+
+        String str2 = (String) null;
+
+        System.out.println(str1);
+
+        System.out.println(null == null);
+
+    }
+}
