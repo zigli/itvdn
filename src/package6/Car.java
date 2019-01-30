ac7438d3ca4edc0f3a89f149d4afb97bd81443a5
ilgiz <4ilgiz@gmail.com>
2019-01-30 15:02:55 +0300

    package6

---
+++ src/package6/Car.java
@@ -0,0 +1,22 @@
+package package6;
+
+public class Car {
+    private int speed;
+    private int price;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
---
+++ src/package6/Main.java
@@ -0,0 +1,9 @@
+package package6;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.setPrice(12000);
+        System.out.println(car.getPrice());
+    }
+}
