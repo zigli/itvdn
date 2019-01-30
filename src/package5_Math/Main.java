ff3218d0c6098017ffc3c4dfa681601b66a564a1
ilgiz <4ilgiz@gmail.com>
2019-01-30 13:57:46 +0300

    Commit package5

--- src/package5_Math/Main.java
+++ src/package5_Math/Main.java
@@ -1,10 +1,14 @@
 package package5_Math;
 
+
+import static java.lang.Math.PI;
+import static java.lang.Math.cos;
+
 public class Main {
     public static void main(String[] args) {
         double num = 1;
-        double res = Math.cos(Math.PI * num);
+        double res = cos(PI * num);
         System.out.println(res);
-        System.out.println(Math.PI);
+        System.out.println(PI);
     }
 }
