be28a6e1d6790e11e06dba40a20758ed255e5516
ilgiz <4ilgiz@gmail.com>
2019-02-04 10:33:07 +0300

    package8

---
+++ src/package8/Animal.java
@@ -0,0 +1,13 @@
+package package8;
+
+public class Animal {
+    private int age;
+
+    private Animal(int age){
+        this.age = age;
+    }
+
+    Animal animal = new Animal(100);
+
+
+}
---
+++ src/package8/Main.java
@@ -0,0 +1,7 @@
+package package8;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+    }
+}
