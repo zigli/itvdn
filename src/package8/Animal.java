5db388529f443992bca7aa2d2d9378075c723038
ilgiz <4ilgiz@gmail.com>
2019-02-04 10:40:01 +0300

    package8 Constructor Overload

--- src/package8/Animal.java
+++ src/package8/Animal.java
@@ -2,12 +2,36 @@ package package8;
 
 public class Animal {
     private int age;
+    private  int height;
 
-    private Animal(int age){
+    public Animal(){
+        this.age = 14;
+        this.height = 100;
+    }
+
+    public Animal(int age){
+        this.age = age;
+        this.height = 133;
+    }
+
+    public Animal(int age, int height){
+        this.age = age;
+        this.height = height;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
     }
 
-    Animal animal = new Animal(100);
+    public int getHeight() {
+        return height;
+    }
 
-
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
--- src/package8/Main.java
+++ src/package8/Main.java
@@ -2,6 +2,12 @@ package package8;
 
 public class Main {
     public static void main(String[] args) {
-        Animal animal = new Animal();
+        Animal animaldefault = new Animal();
+        Animal animalOneParameter = new Animal(22);
+        Animal animalTwoParameter = new Animal( 21,99);
+
+        System.out.println(animaldefault.getAge() + " == " + animaldefault.getHeight());
+        System.out.println(animalOneParameter.getAge() + " == " + animalOneParameter.getHeight());
+        System.out.println(animalTwoParameter.getAge() + " == " + animalTwoParameter.getHeight());
     }
 }
