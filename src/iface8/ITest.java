d5f1d804ad559a03965edf8c29f025bd36b625d3
ilgiz <4ilgiz@gmail.com>
2019-02-15 15:54:50 +0300

    In Java 8 interface methods can have body, but only static or default.

--- src/iface8/ITest.java
+++ src/iface8/ITest.java
@@ -4,4 +4,12 @@ package iface8;
 public interface ITest {
     public static final int PRICE = 10;
     public abstract void move();
+
+    static int sum(){
+        return 5 + 10;
+    }
+
+    default int mult(){
+        return 5 * 10;
+    }
 }
